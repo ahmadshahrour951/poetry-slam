7f2e81f21ba0847e0d8596fb8db505b8ff09aed0
shahrour <ahmadshahrour951@gmail.com>
2020-10-05 12:41:25 -0700

    lines_printed_custom() fn

--- main.py
+++ main.py
@@ -56,3 +56,42 @@ def lines_printed_random(lines_list):
 
 
 # lines_printed_random(get_file_lines('./poem.txt'))
+
+
+def lines_printed_custom(lines_list, loops):
+    """
+    This function takes lines_list and loops as parameters.
+    loops is an integer that will define the number of cycles the user want to perform.
+    A cycle constitutes to printing the list in order then in reverse.
+
+    Parameters:
+    - lines_list: Array - an array of elements of a printable literal
+    - loops: Integer - the number of cycles you want to perform
+
+    Return:
+    -
+    """
+    # This will be used as the range iterator in the upcoming for loop, a simple multiplication helps.
+    half_loops = loops * 2
+    # is_reverse is flag to help direct the logic in upcoming for loop
+    is_reverse = False
+
+    for half_cycle in range(half_loops):
+        # so I'm checking if the flag True, if it isnt print each element in the regular order
+        if not is_reverse:
+            print('CYCLE: ' + str((half_loops/2)))
+            print('====================')
+            for line in range(len(lines_list)):
+                print(lines_list[line])
+            # this will change the flag, so that in the next iteration the logic is navigated to the else statement
+            is_reverse = True
+        else:
+            print('====================')
+            print('REVERSING!')
+            print('====================')
+            # range(len(lines_list) - 1, -1, -1) essentially means go backwards starting at the end of the list
+            for reverse_line in range(len(lines_list) - 1, -1, -1):
+                print(lines_list[reverse_line])
+            print('====================')
+            # this will change the flag, so that in the next iteration the logic is navigated to the first if statement
+            is_reverse = False
